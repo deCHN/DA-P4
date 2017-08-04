930f17759d1b27dd1760d91310db9c551c315eb0
xnc <sehr.jimmy@gmail.com>
2017-08-04 12:15:52 +0200

    answer

--- lesson2-11.R
+++ lesson2-11.R
@@ -1,1 +1,12 @@
-getwd()
+stateinfo = read.csv("./stateData.csv")
+
+state.subset = subset(stateinfo, state.region == 1)
+state.subsetbkt = stateinfo[stateinfo$state.region == 1,]
+
+state.subset == state.subsetbkt
+
+head(state.subset)
+dim(state.subset)
+str(state.subset)
+
+state.aboveAverageMurder = subset(stateinfo, murder > mean(stateinfo$murder))
