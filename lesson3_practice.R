3392ac07227b486e55f74ca11fcf66ea0fbff39a
xnc <sehr.jimmy@gmail.com>
2017-08-17 14:39:14 +0200

    lesson 3 practice

---
+++ lesson3_practice.R
@@ -0,0 +1,131 @@
+# lesson 4 practice
+
+library(ggplot2)
+
+data(diamonds)
+?diamonds
+
+summary(diamonds)
+summary(names(diamonds))
+summary(diamonds$color)
+typeof(diamonds$color)
+
+# Create a histogram of the price of
+# all the diamonds in the diamond data set.
+
+# TYPE YOUR CODE BELOW THE LINE
+# =======================================
+ggplot(aes(x = price), data = diamonds) +
+  geom_histogram(binwidth = 100, color = I('white'), fill = I('#f79420')) +
+  scale_x_continuous(limits = c(0, 19000), breaks = seq(0, 19000, 2000))
+
+summary(diamonds$price)
+
+# how many diamonds cost less than $500?
+nrow(subset(diamonds, price < 500))
+
+# how many diamonds cost less than $250?
+nrow(subset(diamonds, price < 250))
+
+# how many diamonds cost $15000 or more?
+nrow(subset(diamonds, price > 15000 | price == 15000 ))
+
+# Explore the largest peak in the
+# price histogram you created earlier.
+
+# Try limiting the x-axis, altering the bin width,
+# and setting different breaks on the x-axis.
+
+# There won’t be a solution video for this
+# question so go to the discussions to
+# share your thoughts and discover
+# what other people find.
+
+# You can save images by using the ggsave() command.
+# ggsave() will save the last plot created.
+# For example...
+#                  qplot(x = price, data = diamonds)
+#                  ggsave('priceHistogram.png')
+
+# ggsave currently recognises the extensions eps/ps, tex (pictex),
+# pdf, jpeg, tiff, png, bmp, svg and wmf (windows only).
+
+# Submit your final code when you are ready.
+
+# TYPE YOUR CODE BELOW THE LINE
+# ======================================================================
+library(Cairo)
+
+ggplot(aes(x = price), data = diamonds) +
+  geom_histogram(binwidth = 100, color = I('white'), fill = I('#f79420')) +
+  scale_x_continuous(limits = c(0, 19000), breaks = seq(0, 19000, 2000)) +
+  ggsave('diamondPrice.png')
+
+# Break out the histogram of diamond prices by cut.
+# You should have five histograms in separate
+# panels on your resulting plot.
+# TYPE YOUR CODE BELOW THE LINE
+# ======================================================
+ggplot(aes(x = price), data = diamonds) +
+  geom_histogram(binwidth = 100, color = I('white'), fill = I('#f79420')) +
+  scale_x_continuous(limits = c(0, 19000), breaks = seq(0, 19000, 1000)) +
+  facet_grid(cut ~ .)
+
+# price statistics by cut
+by(diamonds$price, diamonds$cut, max)
+by(diamonds$price, diamonds$cut, summary, digits = max(getOption('digits')))
+
+
+# In the two last exercises, we looked at
+# the distribution for diamonds by cut.
+
+# Run the code below in R Studio to generate
+# the histogram as a reminder.
+
+# ===============================================================
+
+qplot(x = price, data = diamonds) + facet_wrap(~cut)
+
+# ===============================================================
+
+# In the last exercise, we looked at the summary statistics
+# for diamond price by cut. If we look at the output table, the
+# the median and quartiles are reasonably close to each other.
+
+# diamonds$cut: Fair
+#    Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+#     337    2050    3282    4359    5206   18570 
+# ------------------------------------------------------------------------ 
+# diamonds$cut: Good
+#    Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+#     327    1145    3050    3929    5028   18790 
+# ------------------------------------------------------------------------ 
+# diamonds$cut: Very Good
+#    Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+#     336     912    2648    3982    5373   18820 
+# ------------------------------------------------------------------------ 
+# diamonds$cut: Premium
+#    Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+#     326    1046    3185    4584    6296   18820 
+# ------------------------------------------------------------------------ 
+# diamonds$cut: Ideal
+#    Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+#     326     878    1810    3458    4678   18810 
+
+# This means the distributions should be somewhat similar,
+# but the histograms we created don't show that.
+
+# The 'Fair' and 'Good' diamonds appear to have 
+# different distributions compared to the better
+# cut diamonds. They seem somewhat uniform
+# on the left with long tails on the right.
+
+# Let's look in to this more.
+
+# Look up the documentation for facet_wrap in R Studio.
+# Then, scroll back up and add a parameter to facet_wrap so that
+# the y-axis in the histograms is not fixed. You want the y-axis to
+# be different for each histogram.
+
+# If you want a hint, check out the Instructor Notes.
+
