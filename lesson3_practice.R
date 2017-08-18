9d6ff89fd3b78e1595f2f953d27031ca43515861
xnc <sehr.jimmy@gmail.com>
2017-08-18 17:15:39 +0200

    u

--- lesson3_practice.R
+++ lesson3_practice.R
@@ -239,4 +239,76 @@ ggplot(aes(x = carat), data = diamonds) +
 
 # Copy and paste all of the code that you used for
 # your investigation, and submit it when you are ready.
+
+# 对于一些 Gapminder 数据集，在转换为 csv 格式后，以下命令可能会很有用： read.csv('data.csv', header = T, row.names = 1, check.names = F)
+# 如果你想对调数据框中的行和列，可以使用这个函数：t()。
 # ====================================================================================
+library(readxl)
+pg <- read_excel('./population-growth.xlsx')
+dim(pg)
+
+tpg <- t(pg)
+
+ggplot(data = pg) +
+  geom_line()
+
+
+# Your task is to investigate the distribution of your friends'
+# birth months and days.
+
+# Here some questions you could answer, and we hope you think of others.
+
+# **********************************************************************
+
+# How many people share your birthday? Do you know them?
+# (Reserve time with them or save money to buy them a gift!)
+
+# Which month contains the most number of birthdays?
+
+# How many birthdays are in each month?
+
+# Which day of the year has the most number of birthdays?
+
+# Do you have at least 365 friends that have birthdays on everyday
+# of the year?
+
+# **********************************************************************
+
+# You will need to do some data munging and additional research to
+# complete this task. This task won't be easy, and you may encounter some
+# unexpected challenges along the way. We hope you learn a lot from it though.
+
+# You can expect to spend 30 min or more on this task depending if you
+# use the provided data or obtain your personal data. We also encourage you
+# to use the lubridate package for working with dates. Read over the documentation
+# in RStudio and search for examples online if you need help.
+
+# You'll need to export your Facebooks friends' birthdays to a csv file.
+# You may need to create a calendar of your Facebook friends’ birthdays
+# in a program like Outlook or Gmail and then export the calendar as a
+# csv file.
+
+# Once you load the data into R Studio, you can use the strptime() function
+# to extract the birth months and birth days. We recommend looking up the
+# documentation for the function and finding examples online.
+
+# We've included some links in the Instructor Notes to help get you started.
+
+# Once you've completed your investigation, create a post in the discussions that includes:
+#       1. any questions you answered, your observations, and summary statistics
+#       2. snippets of code that created the plots
+#       3. links to the images of your plots
+
+# You can save images by using the ggsave() command.
+# ggsave() will save the last plot created.
+# For example...
+#                  qplot(x = price, data = diamonds)
+#                  ggsave('priceHistogram.png')
+
+# ggsave currently recognises the extensions eps/ps, tex (pictex),
+# pdf, jpeg, tiff, png, bmp, svg and wmf (windows only).
+
+# Copy and paste all of the code that you used for
+# your investigation below the line. Submit it when you are ready.
+# ===============================================================================
+?read.csv
